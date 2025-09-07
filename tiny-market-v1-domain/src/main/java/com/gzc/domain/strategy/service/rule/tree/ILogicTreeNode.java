d765d2a89dfb41dd083d886e4726b57f672f2453
gzc <jackiegao6@gmail.com>
2025-09-07 15:26:35 +0800

    feat: 引入规则树模式-处理抽奖中后置规则

---
+++ tiny-market-v1-domain/src/main/java/com/gzc/domain/strategy/service/rule/tree/ILogicTreeNode.java
@@ -0,0 +1,13 @@
+package com.gzc.domain.strategy.service.rule.tree;
+
+
+import com.gzc.domain.strategy.service.rule.tree.factory.DefaultTreeFactory;
+
+/**
+ * @description 规则树接口
+ */
+public interface ILogicTreeNode {
+
+    DefaultTreeFactory.TreeActionEntity logic(String userId, Long strategyId, Integer awardId);
+
+}
