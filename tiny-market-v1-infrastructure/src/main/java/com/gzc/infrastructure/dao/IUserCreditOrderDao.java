4190f69b157a341ef977e8b0d68d14fe3e5b96df
gzc <jackiegao6@gmail.com>
2025-09-08 20:15:18 +0800

    feat: 用于领取活动的库表设计

--- tiny-market-v1-infrastructure/src/main/java/com/gzc/infrastructure/dao/IUserCreditOrderDao.java
+++ tiny-market-v1-infrastructure/src/main/java/com/gzc/infrastructure/dao/IUserCreditOrderDao.java
@@ -4,9 +4,7 @@ import com.gzc.infrastructure.dao.po.UserCreditOrder;
 import org.apache.ibatis.annotations.Mapper;
 
 /**
- * @author Fuzhengwei bugstack.cn @小傅哥
  * @description 用户积分流水单 DAO
- * @create 2024-06-01 08:55
  */
 @Mapper
 //@DBRouterStrategy(splitTable = true)
